431d0f7d4735822fe42bfb9eeb4d5b4ce3809ec3
Paul <paul.salina@gmail.com>
2017-04-13 19:58:08 -0400

    print curvature

--- advanced_lane_finding.py
+++ advanced_lane_finding.py
@@ -88,6 +88,10 @@ def undistort_img(img):
 # Edit this function to create your own pipeline.
 
 def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+    """
+    apply a sobel threshold in the x direction. 
+    apply s channel threshold  
+    """
     img = np.copy(img)
     # Convert to HSV color space and separate the V channel
     hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
@@ -140,6 +144,9 @@ ax2.imshow(transformed, cmap='gray')
 
 
 def warper(img, src, dst):
+    """
+    warp the given image given the source dimensions and destination dimensionså
+    """
 
     # Compute and apply perpective transform
     img_size = (img.shape[1], img.shape[0])
@@ -152,8 +159,10 @@ def warper(img, src, dst):
 
 image = undistort_img(cv2.imread('./test_images/test3.jpg'))
 
+# calculate the source dimensions by approximating the dimensions
+# of the polynomial the covers the lane and its lines
 img_size = np.shape(image)
-ht_window = np.uint(img_size[0] / 1.53)
+ht_window = np.uint(img_size[0] / 1.5)
 hb_window = np.uint(img_size[0])
 c_window = np.uint(img_size[1] / 2)
 ctl_window = c_window - .2 * np.uint(img_size[1] / 2)
@@ -164,6 +173,7 @@ cbr_window = c_window + 1 * np.uint(img_size[1] / 2)
 src = np.float32([[cbl_window, hb_window], [cbr_window, hb_window], [
                  ctr_window, ht_window], [ctl_window, ht_window]])
 
+# this is simply the projection dimension target
 dst = np.float32([[0, img_size[0]], [img_size[1], img_size[0]],
                   [img_size[1], 0], [0, 0]])
 
@@ -187,13 +197,15 @@ f.tight_layout()
 ax2.imshow(warped)
 ax1.imshow(image)
 
-# combine
+# combine the pipeline and then the warping of the image
+# graph of the outcome
 #%%
 image = pipeline(undistort_img(cv2.imread('./test_images/test3.jpg')))
 warped = warper(image, src, dst)
 f, (ax1) = plt.subplots(1, figsize=(24, 9))
 ax1.imshow(warped, cmap="gray")
 
+# show a histogram of a cross section of the image
 #%%
 histogram = np.sum(warped[360:, :], axis=0)
 plt.plot(histogram)
@@ -202,6 +214,9 @@ plt.plot(histogram)
 
 
 def windows(binary_warped):
+    """
+    takes a warped image and return of tuple of (left lane indices, right lane indices)  
+    """
     # Assuming you have created a warped binary image called "binary_warped"
     # Take a histogram of the bottom half of the image
     histogram = np.sum(
@@ -279,6 +294,7 @@ def windows(binary_warped):
     left_fitx = left_fit[0] * ploty**2 + left_fit[1] * ploty + left_fit[2]
     right_fitx = right_fit[0] * ploty**2 + right_fit[1] * ploty + right_fit[2]
 
+    # plot this seperately
     out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
     out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
     plt.imshow(out_img)
@@ -287,5 +303,88 @@ def windows(binary_warped):
     plt.xlim(0, 1280)
     plt.ylim(720, 0)
 
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
 
-windows(warped)
+    window_img = np.zeros_like(out_img)
+    # Color in left and right line pixels
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    # Generate a polygon to illustrate the search window area
+    # And recast the x and y points into usable format for cv2.fillPoly()
+    left_line_window1 = np.array(
+        [np.transpose(np.vstack([left_fitx - margin, ploty]))])
+    left_line_window2 = np.array(
+        [np.flipud(np.transpose(np.vstack([left_fitx + margin, ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array(
+        [np.transpose(np.vstack([right_fitx - margin, ploty]))])
+    right_line_window2 = np.array(
+        [np.flipud(np.transpose(np.vstack([right_fitx + margin, ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+    plt.imshow(result)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+
+    return left_fitx, right_fitx
+
+
+leftx, rightx = windows(warped)
+
+
+#%%
+def fake_lines(leftx, rightx):
+    ploty = np.linspace(0, 719, num=720)  # to cover same y-range as image
+    # quadratic_coeff = 3e-4  # arbitrary quadratic coefficient
+    # # # For each y position generate random x position within +/-50 pix
+    # # # of the line base position in each case (x=200 for left, and x=900 for right)
+    # leftx = np.array([200 + (y**2) * quadratic_coeff + np.random.randint(-50, high=51)
+    #                   for y in ploty])
+    # rightx = np.array([900 + (y**2) * quadratic_coeff + np.random.randint(-50, high=51)
+    #                    for y in ploty])
+
+    # leftx = leftx[::-1]  # Reverse to match top-to-bottom in y
+    # rightx = rightx[::-1]  # Reverse to match top-to-bottom in y
+
+    # Fit a second order polynomial to pixel positions in each fake lane line
+    left_fit = np.polyfit(ploty, leftx, 2)
+    left_fitx = left_fit[0] * ploty**2 + left_fit[1] * ploty + left_fit[2]
+    right_fit = np.polyfit(ploty, rightx, 2)
+    right_fitx = right_fit[0] * ploty**2 + right_fit[1] * ploty + right_fit[2]
+
+    # Plot up the fake data
+    mark_size = 3
+    plt.plot(leftx, ploty, 'o', color='red', markersize=mark_size)
+    plt.plot(rightx, ploty, 'o', color='blue', markersize=mark_size)
+    plt.xlim(0, 1280)
+    plt.ylim(0, 720)
+    plt.plot(left_fitx, ploty, color='green', linewidth=3)
+    plt.plot(right_fitx, ploty, color='green', linewidth=3)
+    plt.gca().invert_yaxis()  # to visualize as we do the images
+
+    y_eval = np.max(ploty)
+    # left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+    # right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+    # print(left_curverad, right_curverad# Define conversions in x and y from pixels space to meters
+    
+    ym_per_pix = 30/720 # meters per pixel in y dimension
+    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+    # Calculate the new radii of curvature
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    # Now our radius of curvature is in meters
+    print(left_curverad, 'm', right_curverad, 'm')
+
+
+fake_lines(leftx, rightx)
